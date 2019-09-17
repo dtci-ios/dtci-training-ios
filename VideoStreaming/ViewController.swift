33da4c065ef8cb5b4aea0b151e4e738d90502841
ja-gutierrez <ja.gutierrez@globant.com>
2019-09-17 11:40:54 -0500

    Initial commit

---
+++ VideoStreaming/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoStreaming
+//
+//  Created by JULIAN ANDRES GUTIERREZ MAYA on 9/16/19.
+//  Copyright © 2019 ESPN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
