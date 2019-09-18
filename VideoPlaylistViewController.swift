01d9f10ba9bd94c61cfa369207d88d7d041e3b94
Juan Cruz Guidi <jguidi@rccl.com>
2019-09-18 17:11:27 -0300

    Created all files necessary for the Video Playlist Table View (added assets for mock data)

---
+++ VideoPlaylistViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoPlaylistViewController.swift
+//  VideoStreaming
+//
+//  Created by Natalia Brasesco on 17/09/2019.
+//  Copyright © 2019 ESPN. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VideoStreaming/VideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoTableViewCell.swift
+//  VideoStreaming
+//
+//  Created by Natalia Brasesco on 18/09/2019.
+//  Copyright © 2019 ESPN. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
