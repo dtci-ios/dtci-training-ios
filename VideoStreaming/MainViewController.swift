cee8f1945c603ec9d82a964f1b10b187dd1faeeb
Rodrigo Cian Berrios <rodrigo.cian@globant.com>
2019-09-19 10:28:07 -0300

    Added implementation.

--- VideoStreaming/MainViewController.swift
+++ VideoStreaming/MainViewController.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Show the progressHUD
+        self.showHUD(progressLabel: "Loading...")
+        
+        //Added asyncAfter on main Queue for see the progressHUD.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            self.mainLabel.text = "Hellow world"
+            //Hide the progressHUD
+            self.dismissHUD(isAnimated: true)
+        }
+    }
+}
+
--- VideoStreaming/UIViewController+MBProgressHUD.swift
+++ VideoStreaming/UIViewController+MBProgressHUD.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import UIKit
+import MBProgressHUD
+ 
+extension UIViewController {
+    func showHUD(progressLabel:String){
+        DispatchQueue.main.async{
+            let progressHUD = MBProgressHUD.showAdded(to: self.view, animated: true)
+            progressHUD.label.text = progressLabel
+        }
+    }
+
+    func dismissHUD(isAnimated:Bool) {
+        DispatchQueue.main.async{
+            MBProgressHUD.hide(for: self.view, animated: isAnimated)
+        }
+    }
+}
