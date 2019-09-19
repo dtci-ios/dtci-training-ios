11a1a3b86b2aea7c7b642c06fc39aaf66816c350
Julian Martin Llorensi <julian.llorensi@globant.com>
2019-09-19 13:32:27 -0300

    Setup video view

--- VideoStreaming/SubViews/Views/VideoView.swift
+++ VideoStreaming/SubViews/Views/VideoView.swift
@@ -9,27 +9,23 @@
 import UIKit
 
 class VideoView: UIView {
+    @IBOutlet var contentView: UIView!
     @IBOutlet private weak var videoImageView: UIImageView!
     @IBOutlet private weak var videoTitle: UILabel!
     @IBOutlet private weak var videoDurationAndDate: UILabel!
     
-    init(frame: CGRect, video: Video) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
-        configure(with: video)
-        setUpImageView()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
     
     func configure(with video: Video) {
-        videoImageView.image = UIImage(named: video.imageName)
-        videoTitle.text = video.title
-        videoDurationAndDate.text = video.duration + "." + video.date.description(with: Locale.current)
-    }
-    
-    private func setUpImageView() {
-        videoImageView.layer.cornerRadius = 10
+        videoImageView?.image = UIImage(named: video.imageName)
+        videoTitle?.text = video.title
+        videoDurationAndDate?.text = video.duration + "." + video.date.description(with: Locale.current)
+        videoImageView?.layer.cornerRadius = 10
     }
 }
