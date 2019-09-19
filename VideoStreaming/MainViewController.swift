c7dfbe8d3ea2222e31b87549b2379ce7a49e6e46
Rodrigo Cian Berrios <rodrigo.cian@globant.com>
2019-09-19 10:23:13 -0300

    Created UIViewController+MBProgressHUD. Created MainViewController for check funcionality of MBProgressHU. Deleted useless ViewController.swift.

---
+++ VideoStreaming/MainViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainViewController.swift
+//  VideoStreaming
+//
+//  Created by Rodrigo Cian Berrios on 18/09/2019.
+//  Copyright © 2019 ESPN. All rights reserved.
+//
+
+import Foundation
---
+++ VideoStreaming/UIViewController+MBProgressHUD.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+MBProgressHUD.swift
+//  VideoStreaming
+//
+//  Created by Rodrigo Cian Berrios on 18/09/2019.
+//  Copyright © 2019 ESPN. All rights reserved.
+//
+
+import Foundation
