c3adfddbb0e3ff248642e832c90bf23fbd464f02
Rodrigo Cian Berrios <rodrigo.cian@globant.com>
2019-09-20 14:27:14 -0300

    Removed bad implementation of 'DispatchQueue.main ..'

--- VideoStreaming/UIViewController+MBProgressHUD.swift
+++ VideoStreaming/UIViewController+MBProgressHUD.swift
@@ -12,15 +12,11 @@ import MBProgressHUD
  
 extension UIViewController {
     func showHUD(progressLabel:String){
-        DispatchQueue.main.async{
-            let progressHUD = MBProgressHUD.showAdded(to: self.view, animated: true)
-            progressHUD.label.text = progressLabel
-        }
+        let progressHUD = MBProgressHUD.showAdded(to: self.view, animated: true)
+        progressHUD.label.text = progressLabel
     }
 
     func dismissHUD(isAnimated:Bool) {
-        DispatchQueue.main.async{
-            MBProgressHUD.hide(for: self.view, animated: isAnimated)
-        }
+        MBProgressHUD.hide(for: self.view, animated: isAnimated)
     }
 }
