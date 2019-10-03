50e5226d54b0bac32d2afae08bc7749ff97e8dc3
Julian Martin Llorensi <julian.llorensi@globant.com>
2019-10-03 17:08:15 -0300

    AVPlayerViewController embeded into StreamPlayerViewController

--- VideoStreaming/StreamPlayerViewController.swift
+++ VideoStreaming/StreamPlayerViewController.swift
@@ -11,7 +11,8 @@ import AVKit
 import AVFoundation
 
 class StreamPlayerViewController: UIViewController {
-    private var avPlayer: AVPlayer!
+    private var playerViewController: AVPlayerViewController!
+    private var player: AVPlayer!
     
     private var streamUrl: URL = URL(fileURLWithPath: "")
     
@@ -21,7 +22,9 @@ class StreamPlayerViewController: UIViewController {
     
     init(streamingUrl url: URL) {
         super.init(nibName: StreamPlayerViewController.nibName, bundle: nil)
+        
         streamUrl = url
+        setupPlayerViewController()
     }
     
     required init?(coder: NSCoder) {
@@ -29,14 +32,17 @@ class StreamPlayerViewController: UIViewController {
     }
     
     func play() {
-        let playerViewController = AVPlayerViewController()
-        
-        avPlayer = AVPlayer(url: streamUrl)
-        playerViewController.player = avPlayer
-            
-        present(playerViewController, animated: true) {
-            playerViewController.player?.play()
-        }
+        player = AVPlayer(url: streamUrl)
+        playerViewController.player = player
+        playerViewController.player?.play()
+    }
+    
+    private func setupPlayerViewController() {
+        playerViewController = AVPlayerViewController()
+        addChild(playerViewController)
+        view.addSubview(playerViewController.view)
+        playerViewController.view.frame = view.bounds
+        playerViewController.didMove(toParent: self)
     }
 }
 
