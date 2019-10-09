edc97a6c3931f400e246e856a4da352641ceb1bb
Julian Martin Llorensi <julian.llorensi@globant.com>
2019-10-09 17:11:40 -0300

    Optional url removed

--- VideoStreaming/Networking/PwnRenameAPI.swift
+++ VideoStreaming/Networking/PwnRenameAPI.swift
@@ -17,11 +17,12 @@ class PwnServiceAPI {
     }
     
     private enum Constants {
+        static let twitchURL = "https://twitch.tv/"
         static let serviceURL = "https://pwn.sh/tools/streamapi.py?url="
     }
     
     func fetchStreamingM3U8Urls(completion: @escaping (QualityUrls) -> Void) {
-        Alamofire.request(Constants.serviceURL + createEncodedURL(userName: streamer), parameters: nil, headers: GameStreamsAPI.headers).responseJSON { (response) in
+        Alamofire.request(Constants.serviceURL + Constants.twitchURL + streamer, parameters: nil, headers: GameStreamsAPI.headers).responseJSON { (response) in
             
             guard let dataResponse = response.data else { return }
             
@@ -34,15 +35,5 @@ class PwnServiceAPI {
                 print("Error when decode JSON \(error.localizedDescription)")
             }
         }
-    }
-    
-    private func createEncodedURL(userName: String) -> String {
-        var urlComponents = URLComponents()
-        
-        urlComponents.scheme = "https"
-        urlComponents.host = "twitch.tv"
-        urlComponents.path = userName
-
-        return urlComponents.url?.absoluteString ?? ""
     }
 }
