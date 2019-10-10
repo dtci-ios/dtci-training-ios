5fe8cafd1096de2f221d9ee27faf9a844469c2e1
Julian Martin Llorensi <julian.llorensi@globant.com>
2019-10-10 12:20:50 -0300

    Unused model QualityUrls removed

---
+++ VideoStreaming/Networking/PwnResponseAPI.swift
@@ -0,0 +1,55 @@
+//
+//  PwnAPI.swift
+//  VideoStreaming
+//
+//  Created by Julian Llorensi on 09/10/2019.
+//  Copyright © 2019 ESPN. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class PwnServiceAPI {
+    var requestURL: String
+    var streamer: String
+    
+    private enum Constants {
+        static let twitchURL = "https://twitch.tv"
+        static let serviceURL = "https://pwn.sh/tools/streamapi.py"
+    }
+    
+    init?(forUser user: String) {
+        streamer = user
+        
+        guard let twitchURL = URL(string: Constants.twitchURL) else { return nil }
+        
+        var componentsForTwitchURL = URLComponents(url: twitchURL, resolvingAgainstBaseURL: false)
+        
+        componentsForTwitchURL?.percentEncodedPath = "/\(streamer.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")"
+        
+        guard let serviceURL = URL(string: Constants.serviceURL) else { return nil }
+
+        var componentsForServiceURL = URLComponents(url: serviceURL, resolvingAgainstBaseURL: false)
+        
+        componentsForServiceURL?.queryItems = [
+            URLQueryItem(name: "url", value: componentsForTwitchURL?.url?.absoluteString ?? "")
+        ]
+        
+        requestURL = componentsForServiceURL?.url?.absoluteString ?? ""
+    }
+    
+    func fetchStreamingM3U8Urls(completion: @escaping (PwnResponse.QualityUrls) -> Void) {
+        Alamofire.request(requestURL, parameters: nil, headers: GameStreamsAPI.headers).responseJSON { (response) in
+            guard let dataResponse = response.data else { return }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let pwnResponse = try decoder.decode(PwnResponse.self, from: dataResponse)
+                completion(pwnResponse.urls)
+            } catch let error {
+                print("Error when decode JSON \(error.localizedDescription)")
+            }
+        }
+    }
+}
--- VideoStreaming/VideoPlaylistViewController.swift
+++ VideoStreaming/VideoPlaylistViewController.swift
@@ -101,18 +101,9 @@ extension VideoPlaylistViewController: UITableViewDelegate, UITableViewDataSourc
         
         let pwnServiceAPI = PwnServiceAPI(forUser: streamUserName)
     
-        pwnServiceAPI.fetchStreamingM3U8Urls { (urls) in
-            let urlsMirror = Mirror(reflecting: urls)
+        pwnServiceAPI?.fetchStreamingM3U8Urls { (urls) in
             
-            var urlsValues: [String] = []
-            
-            urlsMirror.children.forEach { (urlsProperty) in
-                if let urlValue = urlsProperty.value as? String {
-                    urlsValues.append(urlValue)
-                }
-            }
-            
-            guard let lastStreamingUrl = urlsValues.last, let url = URL(string: lastStreamingUrl) else { return }
+            guard let lastStreamingUrl = urls[urls.keys.first ?? ""], let url = URL(string: lastStreamingUrl) else { return }
             
             let streamPlayerViewController = StreamPlayerViewController(streamingUrl: url)
                 
