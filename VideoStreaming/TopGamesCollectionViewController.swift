c15c61c7ac6e916fa7b9c5a4aec2829089788f3c
Rodrigo Cian Berrios <rodrigo.cian@globant.com>
2019-10-14 17:16:23 -0300

    Removed extra classes form TopGamesCollectionViewController file

--- VideoStreaming/TopGamesCollectionViewController.swift
+++ VideoStreaming/TopGamesCollectionViewController.swift
@@ -8,131 +8,6 @@
 
 import UIKit
 
-class ColumsLayoutHelper {
-
-    static func widthOfSafeArea() -> CGFloat {
-        guard let rootView = (UIApplication.shared.windows.filter {$0.isKeyWindow}.first) else { return 0 }
-
-        if #available(iOS 11.0, *) {
-            let leftInset = rootView.safeAreaInsets.left
-            let rightInset = rootView.safeAreaInsets.right
-            return rootView.bounds.width - leftInset - rightInset
-        } else {
-            return rootView.bounds.width
-        }
-    }
-
-    static func heightOfSafeArea() -> CGFloat {
-        guard let rootView = (UIApplication.shared.windows.filter {$0.isKeyWindow}.first) else { return 0 }
-
-        if #available(iOS 11.0, *) {
-            let topInset = rootView.safeAreaInsets.top
-            let bottomInset = rootView.safeAreaInsets.bottom
-            return rootView.bounds.height - topInset - bottomInset
-        } else {
-            return rootView.bounds.height
-        }
-    }
-
-    static func columnsForTraitCollection() -> Int {
-        switch UITraitCollection.current.horizontalSizeClass {
-        case .compact:
-            return UITraitCollection.current.verticalSizeClass == .compact ? 3 : 2
-        case .regular:
-            return UITraitCollection.current.verticalSizeClass == .compact ? 3 : 4
-        default:
-            return 2
-        }
-    }
-}
-
-struct CellAspectRatio {
-    // Portrait mode => 4:3 is 4 heigth and 3 width
-    var width: Int
-    var height: Int
-
-    init(width: Int = 3, height: Int = 4) {
-        self.width = width
-        self.height = height
-    }
-
-    var heightRatioFactor: Float {
-        return Float(height) / Float(width)
-    }
-
-    var widthRatioFactor: Float {
-        return Float(width) / Float(height)
-    }
-}
-
-struct ColumsLayout {
-
-    let cellAspectRatio: CellAspectRatio
-
-    var itemsWidthPercentage: Float // in range of 0..1
-
-    init(itemsWidthPercentage: Float = 0.88, cellAspectRatio: CellAspectRatio = CellAspectRatio()) {
-        let normalizedItemsWidthPercentage = itemsWidthPercentage > 1 ? itemsWidthPercentage / 100 : itemsWidthPercentage
-        self.itemsWidthPercentage = normalizedItemsWidthPercentage
-        self.cellAspectRatio = cellAspectRatio
-    }
-
-    var paddingsWidthPercentage: Float { // in range of 0..1
-        1 - itemsWidthPercentage
-    }
-
-    var fullDisponibleWidthForItems : CGFloat {
-        floor(ColumsLayoutHelper.widthOfSafeArea() * CGFloat(itemsWidthPercentage))
-    }
-
-    var fullDisponibleWidthForPaddings : CGFloat {
-        floor(ColumsLayoutHelper.widthOfSafeArea() - fullDisponibleWidthForItems)
-    }
-
-    var itemWidth : CGFloat {
-       floor(fullDisponibleWidthForItems / CGFloat(ColumsLayoutHelper.columnsForTraitCollection()))
-    }
-
-    var itemHeight : CGFloat {
-        floor(itemWidth * CGFloat(cellAspectRatio.heightRatioFactor))
-    }
-
-    var paddingWidth : CGFloat {
-       floor(fullDisponibleWidthForPaddings / CGFloat(ColumsLayoutHelper.columnsForTraitCollection() + 1))
-    }
-
-    func itemSize() -> CGSize {
-        return CGSize(width: itemWidth, height: itemHeight)
-    }
-}
-
-class TopGamesDataSource: NSObject {
-
-    private var topGamesAPI: TopGamesAPIProtocol
-    private var games: [Game] = []
-
-    func getGames() -> [Game] {
-        return games
-    }
-
-    init(topGamesAPI: TopGamesAPIProtocol){
-        self.topGamesAPI = topGamesAPI
-    }
-
-    func fetchDataSource(completionForView: @escaping (APIError?) -> Void) {
-        topGamesAPI.fetchTopGames { result in
-            switch result {
-            case .success(let topGames):
-                self.games = topGames
-                completionForView(nil)
-            case .failure(let error):
-                self.games = []
-                completionForView(error)
-            }
-        }
-    }
-}
-
 class TopGamesCollectionViewController: UIViewController {
 
     @IBOutlet private weak var collectionView: UICollectionView!
@@ -159,13 +34,13 @@ class TopGamesCollectionViewController: UIViewController {
         title = "Top Games"
 
         showHUD()
-
+        
         collectionView.register(UINib(nibName: GameCollectionViewCell.Constants.nibName, bundle: nil), forCellWithReuseIdentifier: GameCollectionViewCell.Constants.reuseIdentifier)
-
-        dataSource.fetchDataSource(completionForView: errorCompletionHandler(error:))
-
+        
         collectionView.delegate = self
-        collectionView.dataSource = self
+        collectionView.dataSource = dataSource
+        
+        dataSource.fetchDataSource(completionForView: errorCompletionHandler(error:))
     }
 
     func errorCompletionHandler(error: APIError?) {
@@ -186,10 +61,10 @@ class TopGamesCollectionViewController: UIViewController {
     }
 }
 
-extension TopGamesCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+extension TopGamesCollectionViewController: UICollectionViewDelegate {
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let cell = self.collectionView(collectionView, cellForItemAt: indexPath) as! GameCollectionViewCell
+        let cell = self.collectionView.cellForItem(at: indexPath) as! GameCollectionViewCell
 
         guard let game = cell.game else { return }
 
@@ -198,19 +73,6 @@ extension TopGamesCollectionViewController: UICollectionViewDelegate, UICollecti
         navigationController?.pushViewController(videoPlaylistVC, animated: true)
     }
 
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.dataSource.getGames().count
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let viewCell = collectionView.dequeueReusableCell(withReuseIdentifier: GameCollectionViewCell.Constants.reuseIdentifier, for: indexPath) as? GameCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-
-        viewCell.game = self.dataSource.getGames()[indexPath.row]
-
-        return viewCell
-    }
 }
 
 extension TopGamesCollectionViewController: UICollectionViewDelegateFlowLayout {
