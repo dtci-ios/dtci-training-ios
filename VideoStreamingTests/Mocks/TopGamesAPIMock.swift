224218cce91c848881385981a223757588042b25
Rodrigo Cian Berrios <rodrigo.cian@globant.com>
2019-10-14 17:25:43 -0300

    Created TopGamesAPIMock for using on testing

---
+++ VideoStreamingTests/Mocks/TopGamesAPIMock.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  VideoStreamingTests
+//
+//  Created by Rodrigo Cian Berrios on 14/10/2019.
+//  Copyright © 2019 ESPN. All rights reserved.
+//
+
+import Foundation
+@testable import VideoStreaming
+
+class TopGamesAPIMock: NetworkManager, TopGamesAPIProtocol {
+
+    var request = ""
+
+    var fetchDataResultOfCompletion: Swift.Result<[Game],APIError>
+
+    init(_ fetchDataResultOfCompletion: Swift.Result<[Game],APIError>) {
+        self.fetchDataResultOfCompletion = fetchDataResultOfCompletion
+    }
+
+    func fetchTopGames(completion: @escaping (Result<[Game],APIError>) -> Void) {
+        completion(fetchDataResultOfCompletion)
+    }
+}
