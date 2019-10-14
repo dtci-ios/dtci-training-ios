0cad2df7605e4a566d460a33c40610285e5fe81c
Rodrigo Cian Berrios <rodrigo.cian@globant.com>
2019-10-14 17:24:55 -0300

    Created TopGamesDataSource for apply dependency injection on TopGamesCollectionViewController

---
+++ VideoStreaming/TopGamesDataSource.swift
@@ -0,0 +1,53 @@
+//
+//  TopGamesDataSource.swift
+//  VideoStreaming
+//
+//  Created by Rodrigo Cian Berrios on 14/10/2019.
+//  Copyright © 2019 ESPN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TopGamesDataSource: NSObject, UICollectionViewDataSource {
+
+    private var topGamesAPI: TopGamesAPIProtocol
+    private var games: [Game] = []
+
+    func getGames() -> [Game] {
+        return games
+    }
+
+    init(topGamesAPI: TopGamesAPIProtocol){
+        self.topGamesAPI = topGamesAPI
+    }
+
+    func fetchDataSource(completionForView: @escaping (APIError?) -> Void) {
+        topGamesAPI.fetchTopGames { result in
+            switch result {
+            case .success(let topGames):
+                self.games = topGames
+                completionForView(nil)
+            case .failure(let error):
+                self.games = []
+                completionForView(error)
+            }
+        }
+    }
+    
+    //UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let viewCell = collectionView.dequeueReusableCell(withReuseIdentifier: GameCollectionViewCell.Constants.reuseIdentifier, for: indexPath) as? GameCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        viewCell.game = games[indexPath.row]
+
+        return viewCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return games.count
+    }
+}
+
