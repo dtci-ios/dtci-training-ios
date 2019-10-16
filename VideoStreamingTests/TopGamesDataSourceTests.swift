549675c71066fe11143d59bd5c1a445aafbab4aa
Rodrigo Cian Berrios <rodrigo.cian@globant.com>
2019-10-16 12:51:14 -0300

    Refactored TopGamesDataSourceTests

--- VideoStreamingTests/TopGamesDataSourceTests.swift
+++ VideoStreamingTests/TopGamesDataSourceTests.swift
@@ -12,9 +12,7 @@ import XCTest
 class TopGamesDataSourceTests: XCTestCase {
 
     var sut: TopGamesDataSource!
-    
-    var collectionView: UICollectionView!
-    let layout = UICollectionViewFlowLayout()
+
     var gamesMock: [Game]!
     var fetchDataResultOfCompletionMock: Swift.Result<[Game],APIError>!
     
@@ -22,57 +20,131 @@ class TopGamesDataSourceTests: XCTestCase {
         gamesMock = [Game(id: "123", name: "456", boxArtUrl: "www.Aanypic.com"),
                      Game(id: "789", name: "012", boxArtUrl: "www.Banypic.com"),
                      Game(id: "345", name: "678", boxArtUrl: "www.Canypic.com")]
-        
         fetchDataResultOfCompletionMock = .success(gamesMock)
-        
-        collectionView = UICollectionView(frame: CGRect(), collectionViewLayout: layout)
-
-        collectionView.register(UINib(nibName: GameCollectionViewCell.Constants.nibName, bundle: nil), forCellWithReuseIdentifier: GameCollectionViewCell.Constants.reuseIdentifier)
         
         sut = TopGamesDataSource(topGamesAPI: TopGamesAPIMock(fetchDataResultOfCompletionMock))
 
-        collectionView.dataSource = sut
-        
         sut.fetchDataSource{ error in return }        
     }
     
     override func tearDown() {
         gamesMock = nil
         fetchDataResultOfCompletionMock = nil
-        collectionView = nil
         sut = nil
     }
 
-    //testear que no sea nil luego de crear
-    func testTopGamesDataSourceNotNil() {
+    func testSutNotNil() {
         XCTAssertNotNil(sut)
     }
 
-    //numberOfItemsInSection(compara cantidad de elementos con contidad de games de la api)
-    func testNumberOfItemsInSection() {
-        XCTAssertEqual(sut.collectionView(collectionView, numberOfItemsInSection: 0), gamesMock.count)
+    func testHasAnyGame() {
+        XCTAssertEqual(sut.hasAnyGame(), true)
+        sut.clearGames()
+        XCTAssertEqual(sut.hasAnyGame(), false)
     }
-    
-    //cellForItemAt(comoparar tipo de la celda que retorna, compara .game de la celda con game de la API)
-    func testcellForItemAtCellType() {
-        let cell = sut.collectionView(collectionView, cellForItemAt: IndexPath(item: 0, section: 0))
-        XCTAssert(cell is GameCollectionViewCell)
+
+    func testIsGamesEmpty() {
+        XCTAssertEqual(sut.isGamesEmpty(), false)
+        sut.clearGames()
+        XCTAssertEqual(sut.isGamesEmpty(), true)
     }
- 
-    func testcellForItemAtCellItemGame() {
-        let firstCell = sut.collectionView(collectionView, cellForItemAt: IndexPath(item: 0, section: 0)) as! GameCollectionViewCell
-        XCTAssertEqual(firstCell.game, gamesMock.first)
-        let lastCell = sut.collectionView(collectionView, cellForItemAt: IndexPath(item: gamesMock.count - 1 , section: 0)) as! GameCollectionViewCell
-        XCTAssertEqual(lastCell.game, gamesMock.last)
+
+    func testGetGamesCount() {
+        XCTAssertEqual(sut.getGamesCount(), gamesMock.count)
+
+        sut.addGameAt(Game(), position: Int.random(in: 0..<gamesMock.count - 1))
+        XCTAssertEqual(sut.getGamesCount(), gamesMock.count + 1)
+
+        sut.removeGameAt(Int.random(in: 0..<gamesMock.count))
+        XCTAssertEqual(sut.getGamesCount(), gamesMock.count)
+
+        sut.clearGames()
+        XCTAssertEqual(sut.getGamesCount(), 0)
     }
-    
-    //analizar fetchDataSource (tipos de retorno, .failure .success)
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testGetGameAt() {
+        let randomPosition = Int.random(in: 0..<gamesMock.count - 1)
+        XCTAssertEqual(sut.getGameAt(randomPosition), gamesMock[randomPosition])
     }
-    
+
+    func testGetFirstGame() {
+        XCTAssertEqual(sut.getFirstGame(), gamesMock[0])
+    }
+
+    func testGetLastGame() {
+        XCTAssertEqual(sut.getLastGame(), gamesMock[2])
+    }
+
+    func testClearGames() {
+        XCTAssertEqual(sut.isGamesEmpty(), false)
+        sut.clearGames()
+        XCTAssertEqual(sut.isGamesEmpty(), true)
+    }
+
+    func testAddFirstGame() {
+        let firstGame = Game(id: "1", name: "first", boxArtUrl: "www.first.com")
+        XCTAssertNotEqual(sut.getFirstGame(), firstGame)
+        sut.addFirstGame(firstGame)
+        XCTAssertEqual(sut.getFirstGame(), firstGame)
+    }
+
+    func testAddLastGame() {
+        let lastGame = Game(id: "999", name: "last", boxArtUrl: "www.last.com")
+        XCTAssertNotEqual(sut.getLastGame(), lastGame)
+        sut.addLastGame(lastGame)
+        XCTAssertEqual(sut.getLastGame(), lastGame)
+    }
+
+    func testAddGameAt() {
+        let aGame = Game(id: "357", name: "a Game", boxArtUrl: "www.agame.com")
+        let randomPosition = Int.random(in: 0..<gamesMock.count - 1)
+
+        XCTAssertNotEqual(sut.getGameAt(randomPosition), aGame)
+        sut.addGameAt(aGame, position: randomPosition)
+        XCTAssertEqual(sut.getGameAt(randomPosition), aGame)
+    }
+
+    func testContainsGame() {
+        let containedGame = gamesMock[1]
+        let notContainedGame = Game(id: "365412", name: "not contained game", boxArtUrl: "www.not.com")
+
+        XCTAssertEqual(sut.containsGame(containedGame), true)
+        XCTAssertEqual(sut.containsGame(notContainedGame), false)
+    }
+
+    func testRemoveFirstGame() {
+        let firstGame = sut.getGameAt(0)
+        let secondGame = sut.getGameAt(1)
+
+        sut.removeFirstGame()
+        XCTAssertNotEqual(sut.getFirstGame(), firstGame)
+        XCTAssertEqual(sut.getFirstGame(), secondGame)
+    }
+
+
+    func testRemoveLastGame() {
+        let lastGame = sut.getGameAt(sut.getGamesCount() - 1)
+        let gameBeforeLast = sut.getGameAt(sut.getGamesCount() - 2)
+
+        sut.removeLastGame()
+        XCTAssertNotEqual(sut.getLastGame(), lastGame)
+        XCTAssertEqual(sut.getLastGame(), gameBeforeLast)
+    }
+
+    func testRemoveGameAt() {
+        let randomPosition = Int.random(in: 0..<gamesMock.count - 1)
+        let randomGame = sut.getGameAt(randomPosition)
+
+        sut.removeGameAt(randomPosition)
+        XCTAssertNotEqual(sut.getGameAt(randomPosition), randomGame)
+    }
+
+    func testRemoveGame() {
+        let randomPosition = Int.random(in: 0..<gamesMock.count - 1)
+        let randomGame = sut.getGameAt(randomPosition)!
+
+        XCTAssertEqual(sut.containsGame(randomGame), true)
+        sut.removeGame(randomGame)
+        XCTAssertEqual(sut.containsGame(randomGame), false)
+    }
 }
