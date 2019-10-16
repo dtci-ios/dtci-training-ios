07bd8591466e15a828cf6e0234d6ea4ef3f22d03
Julian Martin Llorensi <julian.llorensi@globant.com>
2019-10-16 14:56:56 -0300

    some work added

--- VideoStreaming/SubViews/Views/VideoView.swift
+++ VideoStreaming/SubViews/Views/VideoView.swift
@@ -16,6 +16,11 @@ class VideoView: UIView, NibLoadable {
     @IBOutlet private weak var videoDurationAndDate: UILabel!
     @IBOutlet private weak var liveLabel: UILabel!
     
+    private enum Constants {
+        static let nibName = String(describing: Self.self)
+        static let placeholderImageName = "noImage"
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         nibSetup()
