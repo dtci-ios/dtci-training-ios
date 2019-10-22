2e6d91f9cf8ded3f01530c559b16551e3a29ecb7
Julian Martin Llorensi <julian.llorensi@globant.com>
2019-10-22 12:17:52 -0300

    Comment removed

--- VideoStreaming/StreamPlayerViewController.swift
+++ VideoStreaming/StreamPlayerViewController.swift
@@ -79,9 +79,7 @@ class StreamPlayerViewController: UIViewController {
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-        
-        // let playerLayer = AVPlayerLayer(player: player)
-        
+
         if landscapeVideoView == nil {
             landscapeVideoView  = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.height, height: view.frame.width))
             view.addSubview(landscapeVideoView)
