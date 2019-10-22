d3a9b8c5613e8d8751610026f82e731e805172f4
Julian Martin Llorensi <julian.llorensi@globant.com>
2019-10-22 09:49:43 -0300

    Some functions added to the DataSource

--- VideoStreaming/DataSources/StreamPlayerDataSource.swift
+++ VideoStreaming/DataSources/StreamPlayerDataSource.swift
@@ -8,12 +8,57 @@
 
 import Foundation
 
+import Foundation
+
 class StreamPlayerDataSource {
-    private var relatedVideos: [Video]
+    private var relatedVideos: [Video] = []
+    private var apiManager: VideosAPI
     private var videoTitle: String
-    private var userId: String
-    
-    func getStream(withRow indexRow: Int) -> Video? {
+    private var userId: String?
+
+    var relatedVideosCount: Int {
+        return relatedVideos.count
+    }
+
+    init(apiManager: VideosAPI, videoTitle: String, userId: String) {
+        self.apiManager = apiManager
+        self.videoTitle = videoTitle
+        self.userId = userId
+    }
+
+    func loadData(completion: @escaping (APIError?) -> Void) {
+        guard let userId = userId else {
+            completion(.responseDataNil)
+            return
+        }
+
+        apiManager.fetchVideos(by: userId) { result in
+            switch result {
+            case .success(let retrievedVideos):
+                relatedVideos = retrievedVideos
+                completion(nil)
+            case .failure(let error):
+                completion(error)
+            }
+        }
+    }
+
+    func release() -> Bool {
+        relatedVideos.removeAll()
+        userId = nil
+        return (relatedVideos.count == 0 && userId == nil) ? true : false
+    }
+
+    func getVideo(with videoId: String) -> Video? {
+        let indexRow = containsVideo(with: videoId) ?? -1
+        return getVideo(with: indexRow)
+    }
+
+    func containsVideo(with videoId: String) -> Int? {
+        return relatedVideos.firstIndex { $0.id == videoId }
+    }
+
+    private func getVideo(with indexRow: Int) -> Video? {
         return indexRow >= 0 && indexRow < relatedVideos.count ? relatedVideos[indexRow] : nil
     }
 }
